2b28987869f1e57786eda6aa85e46ee812a679f6
Miłosz Piechocki <miloszpp@gmail.com>
2018-01-10 21:07:24 +0100

    Replaced subscribe call with Async Pipe and added basic loading indicator

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -2,17 +2,16 @@ import { Component } from '@angular/core';
 
 import { BandDataService } from '../band-data.service';
 import { Band } from '../model';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-band-list',
   templateUrl: 'band-list.component.html'
 })
 export class BandListComponent {
-  bandList: Band[];
+  bandList$: Observable<Band[]>;
 
   constructor(private bandDataService: BandDataService) {
-    this.bandDataService.getBands().subscribe(bands => {
-      this.bandList = bands;
-    });
+    this.bandList$ = this.bandDataService.getBands();
   }
 }
