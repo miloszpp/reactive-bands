d3bafb62de9ff5a77d4b23e67adbe816a8053a09
Miłosz Piechocki <miloszpp@gmail.com>
2018-02-22 00:12:14 +0100

    Implemented refresh button and loading indicator

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -3,15 +3,31 @@ import { Component } from '@angular/core';
 import { BandDataService } from '../band-data.service';
 import { Band } from '../model';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { map, mergeMap, startWith } from 'rxjs/operators';
+import { merge } from 'rxjs/observable/merge';
 
 @Component({
   selector: 'app-band-list',
   templateUrl: 'band-list.component.html'
 })
 export class BandListComponent {
-  bandList$: Observable<Band[]>;
+  refreshDataClickSubject = new Subject();
+
+  model$: Observable<{ bands: Band[], isLoading: boolean }>;
 
   constructor(private bandDataService: BandDataService) {
-    this.bandList$ = this.bandDataService.getBands();
+    const refreshDataClick$ = this.refreshDataClickSubject.asObservable();
+    const refreshTrigger$ = refreshDataClick$.pipe(
+      startWith({})
+    );
+    const bandList$ = refreshTrigger$.pipe(
+      mergeMap(() => this.bandDataService.getBands())
+    );
+
+    this.model$ = merge(
+      refreshTrigger$.pipe(map(() => ({ bands: [], isLoading: true }))),
+      bandList$.pipe(map(bands => ({ bands: bands, isLoading: false }))),
+    );   
   }
 }
