14bb2fe5c68e2e32ed0360133cab18c5f231f421
Miłosz Piechocki <miloszpp@gmail.com>
2018-07-07 11:19:45 +0200

    Form backup implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -31,6 +32,7 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes),
     ToastrModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [
     BandDataService,
--- src/app/band-create/band-create.component.ts
+++ src/app/band-create/band-create.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { debounceTime, filter } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-band-create',
   templateUrl: './band-create.component.html',
   styles: []
 })
-export class BandCreateComponent {
-  constructor() {}
+export class BandCreateComponent implements OnInit {
+  bandForm: FormGroup;
+
+  readonly localStorageKey = 'band-crate-snapshot';
+
+  constructor(fb: FormBuilder) {
+    const currentYear = new Date().getFullYear();
+    this.bandForm = fb.group({
+      name: ['', Validators.required],
+      formationYear: [currentYear, Validators.max(currentYear)],
+      isActive: false,
+      biography: ''
+    });
+
+    this.bandForm.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe(formState => {
+      localStorage.setItem(this.localStorageKey, JSON.stringify(formState));
+    });
+  }
+
+  ngOnInit(): void {
+    const formStateSerialized = localStorage.getItem(this.localStorageKey);
+    if (formStateSerialized !== null) {
+      const formState = JSON.parse(formStateSerialized);
+      this.bandForm.setValue(formState);
+    }
+  }
 }
